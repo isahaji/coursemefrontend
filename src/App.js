84f9675460a611eb6c9e873f6b890619ec10d9b2
isahaji <isa@isahaji.com>
2021-08-30 10:37:01 +0300

    imported all comps

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
-
+import { useState, useEffect } from 'react';
+import Header from './comp/Header';
+import Footer from './comp/Footer';
+import Cards from './comp/Card';
+import axios from 'axios';
 
 
 function App() {
+  const instance = axios.create({
+    baseURL: 'http://coursemebackend.azurewebsites.net',
+})
+  const [card, setCard] = useState([]);
+
+    useEffect(()=> {
+        async function fetchData() {
+          const req = await instance.get("/cards")
+
+          setCard(req.data);
+        }
+
+        fetchData();
+    }, [instance])
+
+
+
+
   return (
+
     <div>
+    <Header/>
+    <div className="mt-24 pl-14 pr-14 mb-6 gap-3 grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 " >
 
-      Hi there
+    {card.map((datas) => (
+          <Cards
+            key={datas._id}
+            url={datas.url}
+            title={datas.title}
+            Desc={datas.Desc}
+            link={datas.link}
+          />
+        ))}
 
+    </div>
+
+    <Footer />  
+
+  
     </div>
   );
 }
 
 export default App;
+
+
+
