fb1ea81d8c855dc076839c3cf3abc543178e1918
isahaji <isa@isahaji.com>
2021-09-06 09:06:43 +0300

    removed random component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './comp/Header';
 import Footer from './comp/Footer';
 import Cards from './comp/Card';
 import axios from 'axios';
-import Random from './comp/Random';
+
 
 
 function App() {
@@ -44,7 +44,7 @@ function App() {
     </div>
 
 
-        <Random/>
+
 
     <Footer />  
 
